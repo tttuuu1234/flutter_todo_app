64d8dd5803696530961a0a4b0d62990bd33cb93b
tttuuu1234 <yidongyi8@gmail.com>
2020-10-02 21:29:41 +0900

    編集機能の実装

--- lib/domain/todo_domain.dart
+++ lib/domain/todo_domain.dart
@@ -11,9 +11,18 @@ class TodoDomain extends ChangeNotifier {
 
     // todosの配列にtodoを追加
     todos.add(todo);
+    // 状態をなくしている 無くさないと追加ボタンとか押した時にtodoに前に入力したデータが残ってちゃう
     todo = null;
 
     // notifyListeners()でTodoDomainが使用されているChangeNotifyProviderに変更を通知
+    notifyListeners();
+  }
+
+  void editTodo(int index) {
+    // 編集されたtodosの中のtodoのindexを引数でもらって、todosの中のindex番号のtodoを編集したtodoに書き換えている
+    todos[index] = todo;
+    todo = null;
+
     notifyListeners();
   }
 }
--- lib/ui/todo/list.dart
+++ lib/ui/todo/list.dart
@@ -16,63 +16,64 @@ class TodoList extends StatelessWidget {
         appBar: AppBar(
           title: Text('TodoLists'),
           actions: <Widget>[
-            Consumer<TodoDomain>(builder: (context, model, child) {
-              return IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () {
-                  print('todo追加画面開いたよ');
-                  // todoの追加画面をダイアログで開く
-                  showDialog(
-                    context: context,
-                    // 下記はダイアログの外側を押すとダイアログを閉じるようにするかの設定
-                    // barrierDismissible: false,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text(
-                          '追加したいTodoを教えて！',
-                          style: TextStyle(fontSize: 14),
-                        ),
-                        content: Form(
-                          key: _formKey,
-                          child: TextFormField(
-                            // ignore: missing_return
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return '今日する事教えてくれないの。。？';
-                              }
-                            },
-                            onChanged: (String text) {
-                              // TodoDomainのtodoフィールドに、TextFormFieldで打った文字を反映させている
-                              model.todo = text;
-                            },
+            Consumer<TodoDomain>(
+              builder: (context, model, child) {
+                return IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    // todoの追加画面をダイアログで開く
+                    showDialog(
+                      context: context,
+                      // 下記はダイアログの外側を押すとダイアログを閉じるようにするかの設定
+                      // barrierDismissible: false,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text(
+                            '追加したいTodoを教えて！',
+                            style: TextStyle(fontSize: 14),
                           ),
-                        ),
-                        // AlertDialogではボタン関係はactionsで定義しなくてはいけない
-                        actions: <Widget>[
-                          FlatButton(
-                            child: Text('OK'),
-                            onPressed: () {
-                              if (_formKey.currentState.validate()) {
-                                _formKey.currentState
-                                    .save(); // TextFormFieldのonSavedが呼び出される
-                              }
-                              try {
-                                // todoの追加
-                                model.addTodo();
-                                // TodoListsページに戻って、dialogを閉じる
-                                Navigator.pop(context);
-                              } catch (e) {
-                                print('今日する事が入力されていません');
-                              }
-                            },
+                          content: Form(
+                            key: _formKey,
+                            child: TextFormField(
+                              // ignore: missing_return
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return '今日する事教えてくれないの。。？';
+                                }
+                              },
+                              onChanged: (String text) {
+                                // TodoDomainのtodoフィールドに、TextFormFieldで打った文字を反映させている
+                                model.todo = text;
+                              },
+                            ),
                           ),
-                        ],
-                      );
-                    },
-                  );
-                },
-              );
-            })
+                          // AlertDialogではボタン関係はactionsで定義しなくてはいけない
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text('OK'),
+                              onPressed: () {
+                                if (_formKey.currentState.validate()) {
+                                  _formKey.currentState
+                                      .save(); // TextFormFieldのonSavedが呼び出される
+                                }
+                                try {
+                                  // todoの追加
+                                  model.addTodo();
+                                  // TodoListsページに戻って、dialogを閉じる
+                                  Navigator.pop(context);
+                                } catch (e) {
+                                  print('今日する事が入力されていません');
+                                }
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                );
+              },
+            )
           ],
         ),
         body: Consumer<TodoDomain>(
@@ -81,17 +82,64 @@ class TodoList extends StatelessWidget {
               itemCount: model.todos.length,
               itemBuilder: (context, index) {
                 return Card(
-                  child: Container(
-                    padding: EdgeInsets.all(16),
-                    child: Row(
-                      children: [
-                        // RowやColumeの中でFlexibleは使う
-                        Flexible(
-                          child: Text(
-                            model.todos[index],
-                          ),
-                        ),
-                      ],
+                  child: ListTile(
+                    title: Text(
+                      model.todos[index],
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    contentPadding: EdgeInsets.all(8),
+                    trailing: IconButton(
+                      icon: Icon(Icons.edit),
+                      color: Colors.lightBlueAccent,
+                      onPressed: () {
+                        print('編集');
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text(
+                                '追加したTodo間違えちゃった？',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              content: Form(
+                                key: _formKey,
+                                child: TextFormField(
+                                  initialValue: model.todos[index],
+                                  // ignore: missing_return
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return '今日する事教えてくれないの。。？';
+                                    }
+                                  },
+                                  onChanged: (String text) {
+                                    model.todo = text;
+                                  },
+                                ),
+                              ),
+                              // AlertDialogではボタン関係はactionsで定義しなくてはいけない
+                              actions: <Widget>[
+                                FlatButton(
+                                  child: Text('OK'),
+                                  onPressed: () {
+                                    if (_formKey.currentState.validate()) {
+                                      _formKey.currentState
+                                          .save(); // TextFormFieldのonSavedが呼び出される
+                                    }
+                                    try {
+                                      // todoの追加
+                                      model.editTodo(index);
+                                      // TodoListsページに戻って、dialogを閉じる
+                                      Navigator.pop(context);
+                                    } catch (e) {
+                                      print('今日する事が入力されていません');
+                                    }
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
                     ),
                   ),
                 );
