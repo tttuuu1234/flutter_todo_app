34eedc27b539587c9c62c5a5624e7be3fce013c9
tttuuu1234 <yidongyi8@gmail.com>
2020-10-05 12:10:54 +0900

    完了ページに完了したTodoを表示させる実装

--- lib/ui/todo/complete.dart
+++ lib/ui/todo/complete.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 
 class TodoComplete extends StatelessWidget {
+  TodoComplete(this.completeTodo);
+
+  final List completeTodo;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('TodoComplete'),
+      ),
+      body: ListView.builder(
+        itemCount: completeTodo.length,
+        // ignore: missing_return
+        itemBuilder: (context, index) {
+          if (completeTodo[index]['isCompleted']) {
+            return Card(
+              child: ListTile(
+                title: Text(
+                  completeTodo[index]['text'],
+                  style: TextStyle(fontSize: 14),
+                ),
+                contentPadding: EdgeInsets.all(8),
+              ),
+            );
+          }
+        },
       ),
     );
   }
--- lib/ui/todo/list.dart
+++ lib/ui/todo/list.dart
@@ -8,6 +8,13 @@ import 'package:todo_app/ui/todo/incomplete.dart';
 class TodoList extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
 
+  // ignore: missing_return
+  Color getColor(bool isCompleted) {
+    if (isCompleted) {
+      return Colors.amber;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<TodoDomain>(
@@ -83,109 +90,151 @@ class TodoList extends StatelessWidget {
           builder: (context, model, child) {
             return ListView.builder(
               itemCount: model.todos.length,
+              // ignore: missing_return
               itemBuilder: (context, index) {
                 return Card(
-                  child: ListTile(
-                    title: Text(
-                      model.todos[index]['text'],
-                      style: TextStyle(fontSize: 14),
-                    ),
-                    contentPadding: EdgeInsets.all(8),
-                    // ListTileのtrailingに2つアイコンを並べたい時はWrapしてあげる
-                    trailing: Wrap(
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.edit),
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: Text(
-                                    '追加したTodo間違えちゃった？',
-                                    style: TextStyle(fontSize: 14),
-                                  ),
-                                  content: Form(
-                                    key: _formKey,
-                                    child: TextFormField(
-                                      keyboardType: TextInputType.multiline,
-                                      maxLines: null,
-                                      initialValue: model.todos[index]['text'],
-                                      // ignore: missing_return
-                                      validator: (value) {
-                                        if (value.isEmpty) {
-                                          return '今日する事教えてくれないの。。？';
-                                        }
-                                      },
-                                      onChanged: (String text) {
-                                        model.todo = text;
-                                      },
-                                    ),
-                                  ),
-                                  // AlertDialogではボタン関係はactionsで定義しなくてはいけない
-                                  actions: <Widget>[
-                                    FlatButton(
-                                      child: Text('OK'),
-                                      onPressed: () {
-                                        if (_formKey.currentState.validate()) {
-                                          _formKey.currentState
-                                              .save(); // TextFormFieldのonSavedが呼び出される
-                                        }
-                                        try {
-                                          // todoの追加
-                                          model.editTodo(index);
-                                          // TodoListsページに戻って、dialogを閉じる
-                                          Navigator.pop(context);
-                                        } catch (e) {
-                                          print('今日する事が入力されていません');
-                                        }
-                                      },
-                                    ),
-                                  ],
-                                );
-                              },
+                  child: Container(
+                    color: getColor(model.todos[index]['isCompleted']),
+                    child: ListTile(
+                      title: Text(
+                        model.todos[index]['text'],
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      contentPadding: EdgeInsets.all(8),
+                      onLongPress: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  '完了しましたか？',
+                                  style: TextStyle(fontSize: 14),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              // AlertDialogではボタン関係はactionsで定義しなくてはいけない
+                              actions: [
+                                FlatButton(
+                                  onPressed: () {
+                                    model.completeTodo(index);
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('OK'),
+                                ),
+                                FlatButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('NG'),
+                                ),
+                              ],
                             );
                           },
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.delete,
-                            color: Colors.redAccent,
+                        );
+                      },
+                      // ListTileのtrailingに2つアイコンを並べたい時はWrapしてあげる
+                      trailing: Wrap(
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text(
+                                      '追加したTodo間違えちゃった？',
+                                      style: TextStyle(fontSize: 14),
+                                    ),
+                                    content: Form(
+                                      key: _formKey,
+                                      child: TextFormField(
+                                        keyboardType: TextInputType.multiline,
+                                        maxLines: null,
+                                        initialValue: model.todos[index]
+                                            ['text'],
+                                        // ignore: missing_return
+                                        validator: (value) {
+                                          if (value.isEmpty) {
+                                            return '今日する事教えてくれないの。。？';
+                                          }
+                                        },
+                                        onChanged: (String text) {
+                                          model.todo = text;
+                                        },
+                                      ),
+                                    ),
+                                    // AlertDialogではボタン関係はactionsで定義しなくてはいけない
+                                    actions: <Widget>[
+                                      FlatButton(
+                                        child: Text('OK'),
+                                        onPressed: () {
+                                          if (_formKey.currentState
+                                              .validate()) {
+                                            _formKey.currentState
+                                                .save(); // TextFormFieldのonSavedが呼び出される
+                                          }
+                                          try {
+                                            // todoの追加
+                                            model.editTodo(index);
+                                            // TodoListsページに戻って、dialogを閉じる
+                                            Navigator.pop(context);
+                                          } catch (e) {
+                                            print('今日する事が入力されていません');
+                                            if (model.todo == null) {
+                                              Navigator.of(context).pop();
+                                            }
+                                          }
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
                           ),
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: Align(
-                                    alignment: Alignment.center,
-                                    child: Text(
-                                      '本当に削除しますか？',
-                                      style: TextStyle(fontSize: 16),
-                                      textAlign: TextAlign.center,
+                          IconButton(
+                            icon: Icon(
+                              Icons.delete,
+                              color: Colors.redAccent,
+                            ),
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        '本当に削除しますか？',
+                                        style: TextStyle(fontSize: 16),
+                                        textAlign: TextAlign.center,
+                                      ),
                                     ),
-                                  ),
-                                  actions: [
-                                    FlatButton(
-                                      onPressed: () {
-                                        model.deleteTodo(index);
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text('OK'),
-                                    ),
-                                    FlatButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text('NG'),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      ],
+                                    actions: [
+                                      FlatButton(
+                                        onPressed: () {
+                                          model.deleteTodo(index);
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('OK'),
+                                      ),
+                                      FlatButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('NG'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
@@ -193,50 +242,53 @@ class TodoList extends StatelessWidget {
             );
           },
         ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              SizedBox(
-                height: 80,
-                child: DrawerHeader(
-                  child: Text(
-                    'Menu',
-                    style: TextStyle(fontSize: 36, color: Colors.white),
+        drawer: Consumer<TodoDomain>(builder: (context, model, child) {
+          return Drawer(
+            child: ListView(
+              children: <Widget>[
+                SizedBox(
+                  height: 80,
+                  child: DrawerHeader(
+                    child: Text(
+                      'Menu',
+                      style: TextStyle(fontSize: 36, color: Colors.white),
+                    ),
+                    decoration: BoxDecoration(color: Colors.blue),
                   ),
-                  decoration: BoxDecoration(color: Colors.blue),
                 ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Text('Incomplete'),
-                  onTap: () {
-                    // 未完了ページに飛ぶ前にDrawerを閉じている
-                    Navigator.of(context).pop();
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => TodoIncomplete(),
-                      ),
-                    );
-                  },
+                Card(
+                  child: ListTile(
+                    title: Text('Incomplete'),
+                    onTap: () {
+                      // 未完了ページに飛ぶ前にDrawerを閉じている
+                      Navigator.of(context).pop();
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => TodoIncomplete(),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Text('Complete'),
-                  onTap: () {
-                    // 完了ページに飛ぶ前にDrawerを閉じている
-                    Navigator.of(context).pop();
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => TodoComplete(),
-                      ),
-                    );
-                  },
-                ),
-              )
-            ],
-          ),
-        ),
+                Card(
+                  child: ListTile(
+                    title: Text('Complete'),
+                    onTap: () {
+                      // 完了ページに飛ぶ前にDrawerを閉じている
+                      Navigator.of(context).pop();
+                      List completeTodo = model.getCompleteTodo();
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => TodoComplete(completeTodo),
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
